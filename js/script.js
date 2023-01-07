eeed36e6817960eca8b24f64f9e697dc596f75eb
Nancy Chaudhry <nancych14@gmail.com>
2023-01-07 20:19:08 +0500

    Fixed active player and selecting cell

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const gameboardDiv = document.querySelector('.gameboard');
+const player1Name = document.querySelector('.player1');
+const player2Name = document.querySelector('.player2');
 
 // Gameboard Module initial setup
 const gameboard = (() => {
@@ -15,36 +17,48 @@ const gameboard = (() => {
 				cellDiv.setAttribute('data-row', `${row}`);
 				cellDiv.setAttribute('data-col', `${col}`);
 				cellDiv.textContent = gameArray[row][col];
-				cellDiv.addEventListener('click', (e) => {
-					console.log(`Row ${row} Col ${col}`)
-				});
+				cellDiv.addEventListener('click', playGame.selectCell);
+
 				gameboardDiv.appendChild(cellDiv);
 			}
 		}
-	};
+		}
 	return { gameArray, display };
 })();
 
 // Player Factory Function
 const playerFactory = (name, mark) => {
 	const getName = () => name;
-	const setActivePlayer = () => {
-			this.classList.toggle('inactive');
-	}
-	return { name, mark, getName, setActivePlayer };
+	
+	return { name, mark, getName };
 };
 
 // Play Game Module
-// const playGame = (() => {
+const playGame = (function () {
 	
-// 	console.log(``)
-// 	})
-// 	return {}
-// })();
+	function switchPlayer() {
+		if (activePlayer === player1) {
+			player1Name.classList.toggle('inactive');
+			player2Name.classList.toggle('inactive');
+			activePlayer = player2;
+		} else {
+			player1Name.classList.toggle('inactive');
+			player2Name.classList.toggle('inactive');
+			activePlayer = player1;
+		}
+	}
+	function selectCell(e) {
+		e.target.textContent = activePlayer.mark;
+		const row = e.target.dataset.row;
+		const col = e.target.dataset.col;
+		gameboard.gameArray[row][col] = activePlayer.mark;
+		switchPlayer(activePlayer);
+	}
+	return { selectCell }
+})();
 
 const player1 = playerFactory('player1', 'X');
 const player2 = playerFactory('player2', 'O');
-
+let activePlayer = player1;
 gameboard.display();
-gameboard.gameArray;
-console.log();
+
